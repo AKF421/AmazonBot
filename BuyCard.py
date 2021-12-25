f5725edff76320a9a65eabd740e52e380ff02f87
AKF421 <akf421j@gmail.com>
2021-12-24 23:57:21 -0800

    completed recording process and added some easter eggs

--- BuyCard.py
+++ BuyCard.py
@@ -6,30 +6,30 @@ class BuyCard(object):
 
     @staticmethod
     def buy(email):
-        # # opens QuickTime Player
-        # pyautogui.moveTo(825, 765, 1.5)
-        # pyautogui.click()
-        # time.sleep(5)
-        #
-        # # cancels finder
-        # pyautogui.moveTo(900, 510, 1)
-        # pyautogui.click()
-        # time.sleep(1)
-        #
-        # # right click QuickTime Player
-        # pyautogui.moveTo(825, 765, 1.5)
-        # pyautogui.rightClick()
-        # time.sleep(1)
-        #
-        # # starts new Screen Recording
-        # pyautogui.moveTo(890, 590, .5)
-        # pyautogui.click()
-        # time.sleep(1)
-        #
-        # # hits Record
-        # pyautogui.moveTo(820, 675, .5)
-        # pyautogui.click()
-        # time.sleep(1)
+        # opens QuickTime Player
+        pyautogui.moveTo(825, 765, 1.5)
+        pyautogui.click()
+        time.sleep(5)
+
+        # cancels finder
+        pyautogui.moveTo(900, 510, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        # right click QuickTime Player
+        pyautogui.moveTo(825, 765, 1.5)
+        pyautogui.rightClick()
+        time.sleep(1)
+
+        # starts new Screen Recording
+        pyautogui.moveTo(890, 590, .5)
+        pyautogui.click()
+        time.sleep(1)
+
+        # hits Record
+        pyautogui.moveTo(820, 675, .5)
+        pyautogui.click()
+        time.sleep(1)
 
         # opens Google from PyCharm
         pyautogui.moveTo(515, 770, 1.5)
@@ -46,7 +46,7 @@ class BuyCard(object):
         # Go to search bar and type amazon.com
         pyautogui.moveTo(150, 85, 1)
         pyautogui.click()
-        pyautogui.typewrite('amazon.com\n', 0.15)
+        pyautogui.typewrite('amazon.com\n', 0.5)
         time.sleep(5)
 
         # Navigate to gift cards
@@ -78,6 +78,17 @@ class BuyCard(object):
         pyautogui.click()
         time.sleep(3)
 
+        # Backspaces and types in 'Not Jimmy'
+        pyautogui.moveTo(655, 590, 1)
+        time.sleep(1)
+        pyautogui.click()
+
+        for _ in "Jimmy Nguyen":
+            pyautogui.press('backspace')
+            time.sleep(.5)
+
+        pyautogui.typewrite("Not Jimmy :]", .6)
+        time.sleep(1)
         # Moves to email section
         pyautogui.moveTo(550, 525, 1)
         time.sleep(1)
@@ -86,7 +97,7 @@ class BuyCard(object):
 
         # Types email
         # change email to email[0] after testing
-        pyautogui.typewrite(email[0], 0.3)
+        pyautogui.typewrite(email, 0.5)
         time.sleep(1)
 
         # Scrolls back up and clicks 'Buy Now'
@@ -98,7 +109,7 @@ class BuyCard(object):
         # Sign in
         pyautogui.moveTo(635, 440, .2)
         pyautogui.click()
-        time.sleep(7)
+        time.sleep(10)
 
         # Cancel Last Pass
         pyautogui.moveTo(1110, 260, .5)
@@ -107,45 +118,98 @@ class BuyCard(object):
 
         # Buy
         pyautogui.moveTo(1045, 280, .2)
-        time.sleep(5)
-        #
-        # # Stops screen recording
-        # pyautogui.moveTo(765, 13, 1)
-        # pyautogui.click()
-        # time.sleep(2)
-        #
-        # # Exits out of preview
-        # pyautogui.moveTo(63, 39, 1)
-        # time.sleep(1)
-        #
-        # # Reopens Google
-        # pyautogui.moveTo(515, 770, 1.5)
-        # pyautogui.click()
-        # time.sleep(2)
-        #
-        # # Opens new tab and opens google photos
-        # pyautogui.moveTo(355, 50, 1.5)
-        # pyautogui.click()
-        # time.sleep(.2)
-        # pyautogui.typewrite('http://photos.google.com/\n', .1)
-        # time.sleep(5)
-        #
-        # # Opens Finder
-        # pyautogui.moveTo(360, 765, 1)
-        # pyautogui.click()
-        # time.sleep(1)
-        #
-        # # Accesses desktop
-        # pyautogui.moveTo(65, 195, 1)
-        # pyautogui.click()
-        # time.sleep(1)
-        #
-        # # Turns into grid display
-        # pyautogui.moveTo(605, 50, .5)
-        # pyautogui.click()
-        # time.sleep(1)
-        #
-        # # Hovers over .mov file and drags to google photos
-        # pyautogui.moveTo(450, 125, .25)
-        # pyautogui.dragTo(650, 390, 5)
+        time.sleep(1)
+        pyautogui.click()
+        time.sleep(10)
 
+        # Stops screen recording
+        pyautogui.moveTo(655, 13, 1)
+        pyautogui.click()
+        time.sleep(2)
+
+        # Exits out of preview
+        pyautogui.moveTo(63, 39, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Reopens Google
+        pyautogui.moveTo(515, 770, 1.5)
+        time.sleep(1)
+        pyautogui.click()
+        time.sleep(2)
+
+        # Opens new tab and opens google photos
+        pyautogui.moveTo(355, 50, 1.5)
+        pyautogui.click()
+        time.sleep(2)
+        pyautogui.typewrite('photos.google.com\n', .5)
+        time.sleep(5)
+
+        # Opens Finder
+        pyautogui.moveTo(345, 765, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Accesses desktop
+        pyautogui.moveTo(65, 195, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Turns into grid display
+        pyautogui.moveTo(605, 50, .5)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Hovers over .mov file and drags to google photos
+        pyautogui.moveTo(465, 125, .25)
+        time.sleep(2)
+        pyautogui.dragTo(650, 390, 5, button='left')
+        time.sleep(60)
+
+        # Clicks on video and creates shared link
+        pyautogui.moveTo(440, 325, 1)
+        time.sleep(1)
+
+        pyautogui.click()
+        time.sleep(2)
+
+        pyautogui.moveTo(1050, 133, 2)
+        time.sleep(1)
+        pyautogui.click()
+        time.sleep(2)
+
+        pyautogui.moveTo(505, 640, 2)
+        time.sleep(1)
+        pyautogui.click()
+        time.sleep(2)
+
+        pyautogui.moveTo(780, 490, 2)
+        time.sleep(1)
+        pyautogui.click()
+
+        time.sleep(5)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Open discord
+        pyautogui.moveTo(620, 765, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        # Paste link
+        pyautogui.moveTo(415, 690, 1)
+        pyautogui.click()
+        time.sleep(1)
+
+        pyautogui.rightClick()
+        time.sleep(1)
+        pyautogui.moveTo(540, 670, 1)
+        pyautogui.click()
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(1)
+
+        pyautogui.moveTo(415, 690, 1)
+        pyautogui.click()
+        time.sleep(1)
+        pyautogui.typewrite("This was all done by a computer!\n", 0.5)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pyautogui
 # import time
-# import ReadEmail
+import ReadEmail
 from BuyCard import BuyCard
-#
+
 pyautogui.FAILSAFE = True
-# read_email = ReadEmail
+read_email = ReadEmail
 buy_card = BuyCard()
 global email
 
@@ -12,14 +12,16 @@ global email
 def main():
     global email
     email = read_email.values[0]
-    # email = 'nguyen.hoang.jimmy42@gmail.com'
+    email = 'nguyen.hoang.jimmy42@gmail.com'
     BuyCard.buy(email)
 
 
 if __name__ == "__main__":
     main()
 
-# Tool for identifying position of something
-#
-# time.sleep(5)
-# print(pyautogui.position())
+# # Tool for identifying position of something
+# #
+# # time.sleep(5)
+# # pyautogui.scroll(-10)
+# # time.sleep(5)
+# # print(pyautogui.position())
